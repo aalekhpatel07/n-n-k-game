548c75f4560f59b5c48d7754a65f0177821d2265
aalekhpatel07 <aalekh.gwpeck.7998@gmail.com>
2020-07-18 19:51:21 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get()
+
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on http://localhost:${PORT}`);
+});
